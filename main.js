80aeacceace04e5e0749146665eb44077c9a1626
Robert Rice <rrice1291@gmail.com>
2018-08-25 10:27:15 -0500

    object shorthand notation

--- main.js
+++ main.js
@@ -15,3 +15,19 @@ const lastName = 'rice';
 // greetingElement.innerHTML = 'Hello my name is ' + firstName + ' ' + lastName;
 
 greetingElement.innerHTML = `Hello my name is ${firstName} ${lastName}`;
+
+// object shorthand notation
+let name = 'callan';
+let age = '10000000000000';
+
+// let person={
+//     name: name,
+//     age: age
+// };
+
+let person = {
+    name,
+    age
+};
+
+console.log('person: ',person.name);
